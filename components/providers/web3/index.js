c928e20a55d01f275637a767ba163ae680e667c9
Igor-Koniukhov <ikoniukhov@gmail.com>
2022-12-22 17:16:16 +0200

    initialized state for web3Api

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -1,9 +1,15 @@
-import {createContext, useContext, useEffect} from "react";
+import {createContext, useContext, useEffect, useState} from "react";
 import detectEthereumProvider from "@metamask/detect-provider";
 
 const Web3Context = createContext(null)
 
 export default function Web3Provider({children}) {
+    const [web3Api, setWeb3Api]=useState({
+        provider: null,
+        web3: null,
+        contract: null,
+        isInitialized: false
+    })
     useEffect(() => {
         const loadProvider = async () => {
             const provider = await detectEthereumProvider()
